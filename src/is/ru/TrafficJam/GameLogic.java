c0ebee93e01dac1904cb6d4c6c146d97a1f5653d
ThorarinnS <thorarinn.sveinsson@gmail.com>
2013-10-26 16:18:53 +0000

    gamelogic

--- src/is/ru/TrafficJam/GameLogic.java
+++ src/is/ru/TrafficJam/GameLogic.java
@@ -1,5 +1,9 @@
 package is.ru.TrafficJam;
 
+import android.graphics.Point;
+
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: AuðunVetle
@@ -9,6 +13,34 @@ package is.ru.TrafficJam;
  */
 public class GameLogic
 {
+    ArrayList<Block> blockArray;
 
+    public ArrayList<Block> getBlockArray() {
+        return blockArray;
+    }
+    GameLogic(int level){
+        blockArray = new ArrayList<Block>();
+        //TODO: get the level from XMLParser
+    }
 
+    public boolean isGameOver(){
+        if (blockArray.get(0).getPos().equals(4,2))
+            return true;
+        else
+            return false;
+    }
+
+    public void moveBlock (Point blockToMove, Point posToMoveTo){
+        Block block = getBlockByPos(blockToMove);
+        if (block != null){
+            block.setPos(posToMoveTo);
+        }
+    }
+    private Block getBlockByPos(Point pos){
+        for (Block b : blockArray){
+            if (b.getPos() == pos)
+                return b;
+        }
+        return null;
+    }
 }
