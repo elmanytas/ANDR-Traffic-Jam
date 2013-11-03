cd4d7a46230b624b06ae5ec2be78ac37e4596713
Auðun Velte Atlason <Skuggomann+Git@Gmail.com>
2013-11-03 18:53:49 +0000

    Breitti vhernig swithc button virkar

--- src/is/ru/TrafficJam/OptionsActivity.java
+++ src/is/ru/TrafficJam/OptionsActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,14 +17,24 @@ import android.widget.Switch;
  */
 public class OptionsActivity extends Activity {
 
-
+private Switch switchVibrate;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.options);
 
+        switchVibrate = (Switch) findViewById(R.id.switchVibrate);
 
-        MainActivity.settings.getBoolean("vibrate",false);
+        switchVibrate.setChecked(MainActivity.settings.getBoolean("vibrate",false));
+
+        switchVibrate.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Editor editor = MainActivity.settings.edit();
+                editor.putBoolean(getString(R.string.settings_file_name), switchVibrate.isChecked());
+                editor.commit();
+            }
+        });
+
     }
 
 
@@ -35,9 +47,7 @@ public class OptionsActivity extends Activity {
         switch ( view.getId() )
         {
             case R.id.switchVibrate:
-                Editor editor = MainActivity.settings.edit();
-                editor.putBoolean(getString(R.string.settings_file_name), switchState);
-                editor.commit();
+
                 break;
 
         }
